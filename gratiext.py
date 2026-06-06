110f50af5a613754b879ebc4cf075384892a31aa
DLMN3008 <dleon.nunez@gmail.com>
2026-06-05 20:16:01 -0500

    Remove author information from gratiext.py
    Remove author line from the script.

--- gratiext.py
+++ gratiext.py
@@ -2,7 +2,6 @@
 # NIC 19 - GRATIFICACION POR TIEMPO DE SERVICIO
 # PARTE 1 CORREGIDA
 # ==========================================================
-st.write("Autor: DAVID ENRIQUE LEON NUÑEZ")
 
 import streamlit as st
 import pandas as pd
